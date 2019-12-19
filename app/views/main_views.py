fed0bb3216e779842361ad5a53311d27bd575dca
Jack Jiang <jackjiang@utexas.edu>
2019-12-18 19:30:11 -0600

    add google quickstart and barebones code

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -3,54 +3,60 @@
 # Authors: Ling Thio <ling.thio@gmail.com>
 
 
-from flask import Blueprint, redirect, render_template
-from flask import request, url_for
-from flask_user import current_user, login_required, roles_required
+from __future__ import print_function
 
-from app import db
-from app.models.user_models import UserProfileForm
+from flask import Blueprint, render_template
+import datetime
+import pickle
+import os.path
+# from googleapiclient.discovery import build
+# from google_auth_oauthlib.flow import InstalledAppFlow
+# from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.events']
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 
 # The Home page is accessible to anyone
 @main_blueprint.route('/')
 def home_page():
+    # """Shows basic usage of the Google Calendar API.
+    # Prints the start and name of the next 10 events on the user's calendar.
+    # """
+    # creds = None
+    # # The file token.pickle stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
+    # if os.path.exists('token.pickle'):
+    #     with open('token.pickle', 'rb') as token:
+    #         creds = pickle.load(token)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open('token.pickle', 'wb') as token:
+    #         pickle.dump(creds, token)
+
+    # service = build('calendar', 'v3', credentials=creds)
+
+    # # Call the Calendar API
+    # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    # print('Getting the upcoming 10 events')
+    # events_result = service.events().list(calendarId='primary', timeMin=now,
+    #                                     maxResults=10, singleEvents=True,
+    #                                     orderBy='startTime').execute()
+    # events = events_result.get('items', [])
+
+    # if not events:
+    #     print('No upcoming events found.')
+    # for event in events:
+    #     start = event['start'].get('dateTime', event['start'].get('date'))
+    #     print(start, event['summary'])
+        
     return render_template('main/home_page.html')
-
-
-# The User page is accessible to authenticated users (users that have logged in)
-@main_blueprint.route('/member')
-@login_required  # Limits access to authenticated users
-def member_page():
-    return render_template('main/user_page.html')
-
-
-# The Admin page is accessible to users with the 'admin' role
-@main_blueprint.route('/admin')
-@roles_required('admin')  # Limits access to users with the 'admin' role
-def admin_page():
-    return render_template('main/admin_page.html')
-
-
-@main_blueprint.route('/main/profile', methods=['GET', 'POST'])
-@login_required
-def user_profile_page():
-    # Initialize form
-    form = UserProfileForm(request.form, obj=current_user)
-
-    # Process valid POST
-    if request.method == 'POST' and form.validate():
-        # Copy form fields to user_profile fields
-        form.populate_obj(current_user)
-
-        # Save user_profile
-        db.session.commit()
-
-        # Redirect to home page
-        return redirect(url_for('main.home_page'))
-
-    # Process GET or invalid POST
-    return render_template('main/user_profile_page.html',
-                           form=form)
-
 
